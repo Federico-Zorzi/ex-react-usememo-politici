df1a41d8b423914e44897d155aeaf9d0be0f481c
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-03-06 16:01:59 +0100

    filtered politicians list

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,78 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo, memo } from "react";
+
+const PoliticianCard = memo(({ image, name, position, biography }) => {
+  console.log("Render:", name);
+  return (
+    <div className="politician-card">
+      <figure className="politician-card-image">
+        <img src={image ?? ""} alt={name} />
+      </figure>
+      <div className="politician-card-content">
+        <h3>{name}</h3>
+        <span>{position}</span>
+        <p>{biography}</p>
+      </div>
+    </div>
+  );
+});
 
 function App() {
   const [politiciansList, setPoliticiansList] = useState([]);
-  console.log(politiciansList);
+  const [filter, setFilter] = useState("");
+  /* console.log(politiciansList); */
 
-  const newPoliticiansList = () => {
-    fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/politicians`)
-      .then((res) => res.json())
-      .then((pl) => setPoliticiansList(pl));
-  };
-  useEffect(newPoliticiansList, []);
+  useEffect(() => {
+    const newPoliticiansList = async () => {
+      try {
+        const fetchResPoliticians = await fetch(
+          `https://boolean-spec-frontend.vercel.app/freetestapi/politicians`
+        ).then((res) => res.json());
+
+        if (!fetchResPoliticians.length > 0) {
+          throw new Error("Non è disponibile una lista di politici");
+        }
+
+        setPoliticiansList(fetchResPoliticians);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    newPoliticiansList();
+  }, []);
+
+  const filteredPoliticiansList = useMemo(() => {
+    if (!filter.trim()) return politiciansList;
+
+    return politiciansList.filter(
+      (p) =>
+        p.name.toLowerCase().includes(filter.toLowerCase()) ||
+        p.biography.toLowerCase().includes(filter.toLowerCase())
+    );
+  }, [politiciansList, filter]);
 
   return (
     <main>
       <h1>Lista della politici</h1>
 
+      {/* SEARCH FIELD */}
+      <div id="search-field">
+        <input type="text" onChange={(e) => setFilter(e.target.value)} />
+      </div>
+
       <section className="cards-list-politicians">
-        {politiciansList.length > 0 &&
-          politiciansList.map((politician) => {
-            return (
-              <div key={politician.id} className="politician-card">
-                <figure className="politician-card-image">
-                  <img src={politician.image ?? ""} alt={politician.name} />
-                </figure>
-                <div className="politician-card-content">
-                  <h3>{politician.name}</h3>
-                  <span>{politician.position}</span>
-                  <p>{politician.biography}</p>
-                </div>
-              </div>
-            );
-          })}
+        {filteredPoliticiansList.length > 0 &&
+          filteredPoliticiansList.map((politician) => (
+            <PoliticianCard
+              key={politician.id}
+              image={politician.image}
+              name={politician.name}
+              position={politician.position}
+              biography={politician.biography}
+            />
+          ))}
       </section>
     </main>
   );
 }
 
 export default App;
-
-/* 
-Nome (name)
-Immagine (image)
-Posizione (position)
-Breve biografia (biography)
- */
